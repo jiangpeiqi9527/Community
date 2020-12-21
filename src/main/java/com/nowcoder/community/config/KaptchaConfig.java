fbb655899eaf3d951c8f54ff2c714507fbb28596
jxz <1741585872@qq.com>
2020-12-21 14:53:24 +0800

    第二章，完成了验证码生成，解决了jquery导入不进来的bug

---
+++ src/main/java/com/nowcoder/community/config/KaptchaConfig.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KaptchaConfig {
+    /**
+     * producer是kaptcha的核心接口，以下是producer接口中的两个核心方法
+     *      BufferedImage createImage(String var1); 根据文字创建验证码图片
+     *
+     *      String createText(); 创建验证码文字
+     *
+     * DefaultKaptcha是producer接口的默认实现类
+     * @return
+     */
+    @Bean
+    public Producer kaptchaProducer() {
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width","100");
+        properties.setProperty("kaptcha.image.height","40");
+        properties.setProperty("kaptcha.textproducer.font.size","32");
+        properties.setProperty("kaptcha.textproducer.font.color","0,0,0");
+        properties.setProperty("kaptcha.textproducer.char.string","0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        properties.setProperty("kaptcha.textproducer.char.length","4");
+        properties.setProperty("kaptcha.noise.impl","com.google.code.kaptcha.impl.NoNoise");
+
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+}
