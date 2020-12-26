edab013dddf8f39a4669069c3311e18167b4fbc5
jxz <1741585872@qq.com>
2020-12-26 15:52:55 +0800

    第三章，完成了统一异常处理、使用aop完成了统一日志处理（service层）

---
+++ src/main/java/com/nowcoder/community/controller/advice/ServiceLogAspect.java
@@ -0,0 +1,43 @@
+package com.nowcoder.community.controller.advice;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) { // 这个JoinPoint指的是所调用的方法（连接点的目标）
+        // 用户[192.168.0.111]，在[时间]，访问了[com.nowcoder.community.service.xxx()]
+        // 这里利用工具类RequestContextHolder的子类ServletRequestAttributes的getRequest()方法来获取request对象
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        // 得到ip
+        String ip = request.getRemoteHost();
+        // 得到时间
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        // 得到调用的方法
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s]，在[%s]，调用了[%s]方法", ip, now, target));
+    }
+}
