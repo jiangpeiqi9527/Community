ca3ed6c13b1a7436beaada687ebd5c45cb4f60c0
jxz <1741585872@qq.com>
2020-12-27 23:18:58 +0800

    第四章，实现显示个人主页、个人主页收到的赞、重构的点赞代码（使用redis的事务）

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
@@ -43,6 +44,9 @@ public class UserController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private LikeService likeService;
 
     @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
@@ -103,16 +107,33 @@ public class UserController {
             int num = 0;
             while (num != -1) {
                 num = fis.read(buffer);
-                os.write(buffer,0,num);
+                os.write(buffer, 0, num);
             }
         } catch (IOException e) {
             logger.error("读取头像失败：" + e.getMessage());
-        }finally {
+        } finally {
             try {
                 fis.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    // 个人主页
+    @RequestMapping(path = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable("userId") int userId, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在");
+        }
+
+        // 用户
+        model.addAttribute("user",user);
+        // 点赞的数量
+        int likeCount = likeService.findUserLikeCount(userId);
+        model.addAttribute("likeCount",likeCount);
+
+        return "/site/profile";
     }
 }
--- src/main/java/com/nowcoder/community/service/LikeService.java
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -2,7 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,18 +14,46 @@ public class LikeService {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    // 点赞like:entity:entityType:entityId  --> set(userId)
-    public void like(int userId, int entityType, int entityId) {
-        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
-        // 判断集合里有没有userId
-        Boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
-        if (isMember) {
-            // 删除entityLikeKey(set)集合中的userId
-            redisTemplate.opsForSet().remove(entityLikeKey, userId);
-        } else {
-            // 添加entityLikeKey(set)集合里userId
-            redisTemplate.opsForSet().add(entityLikeKey, userId);
-        }
+
+    /**
+     * 点赞like:entity:entityType:entityId  --> set(userId)
+     * 点赞操作
+     *
+     * @param userId     点赞的那个人的id
+     * @param entityType 点赞对象的类型
+     * @param entityId   点赞对象的编号
+     */
+    public void like(int userId, int entityType, int entityId, int entityUserId) {
+        // 由于点赞这个操作需要更新like:entity:entityType:entityId和like:user:userId这两个key，所以需要redis事务操作
+//        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+//        // 判断集合里有没有userId
+//        Boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+//        if (isMember) {
+//            // 删除entityLikeKey(set)集合中的userId
+//            redisTemplate.opsForSet().remove(entityLikeKey, userId);
+//        } else {
+//            // 添加entityLikeKey(set)集合里userId
+//            redisTemplate.opsForSet().add(entityLikeKey, userId);
+//        }
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+                String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
+                boolean isMember = operations.opsForSet().isMember(entityLikeKey, userId);
+                operations.multi();
+
+                if (isMember) {
+                    operations.opsForSet().remove(entityLikeKey, userId);
+                    operations.opsForValue().decrement(userLikeKey);
+                } else {
+                    operations.opsForSet().add(entityLikeKey,userId);
+                    operations.opsForValue().increment(userLikeKey);
+                }
+
+                return operations.exec();
+            }
+        });
     }
 
     // 查询某实体点赞的数量
@@ -36,5 +67,12 @@ public class LikeService {
     public int findEntityLikeStatus(int userId, int entityType, int entityId) {
         String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
         return redisTemplate.opsForSet().isMember(entityLikeKey, userId) ? 1 : 0;
+    }
+
+    // 查询某个用户获得的赞的数量
+    public int findUserLikeCount(int userId) {
+        String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
+        Integer count = (Integer)redisTemplate.opsForValue().get(userLikeKey);
+        return count == null ? 0 : count.intValue();
     }
 }
