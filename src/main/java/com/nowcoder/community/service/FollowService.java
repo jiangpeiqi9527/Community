ee7e521b2968786e1a9946395076ad166726af0f
jxz <1741585872@qq.com>
2020-12-28 15:50:57 +0800

    第四章，实现关注、取消关注功能                需求
                   * 开发关注、取消关注功能。
                   * 统计用户的关注数、粉丝数
    
                   关键
    
                   * 若A关注了B，则A是B的粉丝（follower），B是A的目标（followee）
                   * 关注的目标可以是用户、帖子、题目等，在实现时将这些目标抽象为实体。

---
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -0,0 +1,66 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FollowService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public void follow(int userId, int entityType, int entityId) {
+        // 由于是两次操作,所以要使用事务
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+                operations.multi();
+
+                operations.opsForZSet().add(followeeKey, entityId, System.currentTimeMillis());
+                operations.opsForZSet().add(followerKey, userId, System.currentTimeMillis());
+
+                return operations.exec();
+            }
+        });
+    }
+
+    public void unfollow(int userId, int entityType, int entityId) {
+        // 由于是两次操作,所以要使用事务
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+                operations.multi();
+
+                operations.opsForZSet().remove(followeeKey, entityId);
+                operations.opsForZSet().remove(followerKey, userId);
+
+                return operations.exec();
+            }
+        });
+    }
+
+    // 查询关注的实体的数量
+    public long findFolloweeCount(int userId, int entityType) {
+        return redisTemplate.opsForZSet().zCard(RedisKeyUtil.getFolloweeKey(userId, entityType));
+    }
+
+    // 查询实体的粉丝的数量
+    public long findFollowerCount(int entityType, int entityId) {
+        return redisTemplate.opsForZSet().zCard(RedisKeyUtil.getFollowerKey(entityType, entityId));
+    }
+
+    // 查询当前用户是否已关注该实体
+    public boolean hasFollowed(int userId, int entityType, int entityId) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return redisTemplate.opsForZSet().score(followeeKey,entityId) != null;
+    }
+}
