20e0b923f87f9b78e00eda48b03778fa765ba7ee
jxz <1741585872@qq.com>
2020-12-28 19:49:39 +0800

    第四章，实现了关注列表、粉丝列表
        > 业务层
        >
        > * 查询某个用户关注的人，支持分页
        > * 查询某个用户的粉丝，支持分页
        >
        > 表现层
        >
        > * 处理“查询关注的人”，“查询粉丝”请求
        > * 编写“查询关注得人”、“查询粉丝”模板

--- src/main/java/com/nowcoder/community/controller/FollowController.java
+++ src/main/java/com/nowcoder/community/controller/FollowController.java
@@ -1,23 +1,35 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.FollowService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant{
 
     @Autowired
     private FollowService followService;
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
 
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
@@ -37,5 +49,59 @@ public class FollowController {
         followService.unfollow(user.getId(), entityType, entityId);
 
         return CommunityUtil.getJSONString(0, "已取消关注");
+    }
+
+    // 查询某用户关注得人
+    @RequestMapping(path = "/followees/{userId}", method = RequestMethod.GET)
+    public String getFollowees(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在");
+        }
+        model.addAttribute("user", user);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+        page.setLimit(5);
+        page.setPath("/followees/" + userId);
+
+        List<Map<String, Object>> followees = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if (followees != null) {
+            for (Map<String, Object> map : followees) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed",hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users",followees);
+        return "/site/followee";
+    }
+
+    // 查询某用户的粉丝
+    @RequestMapping(path = "/followers/{userId}", method = RequestMethod.GET)
+    public String getFollowers(@PathVariable("userId") int userId, Page page, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在");
+        }
+        model.addAttribute("user", user);
+        page.setRows((int) followService.findFollowerCount(CommunityConstant.ENTITY_TYPE_USER, userId));
+        page.setLimit(5);
+        page.setPath("/followers/" + userId);
+
+        List<Map<String, Object>> followers = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if (followers != null) {
+            for (Map<String, Object> map : followers) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed",hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users",followers);
+        return "/site/follower";
+    }
+
+
+    private boolean hasFollowed(int userId) {
+        if (hostHolder.getUser() == null) {
+            return false;
+        }
+        return followService.hasFollowed(hostHolder.getUser().getId(), CommunityConstant.ENTITY_TYPE_USER, userId);
     }
 }
