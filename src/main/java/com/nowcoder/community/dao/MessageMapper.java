e01d54af018a1ddb3129fa5b9b344f6075e6578c
jxz <1741585872@qq.com>
2021-01-02 19:51:31 +0800

    第五章，# 显示系统的通知
        > 通知列表
        >
        > * 显示评论、点赞、关注三种类型的通知
        >
        > 显示详情
        >
        > * 分页显示某一类主题所包含的通知
        >
        > 未读消息
        >
        > * 在页面头部显示所有的未读消息数量

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -37,4 +37,7 @@ public interface MessageMapper {
 
     // 未读的通知数量
     int selectNoticeUnreadCount(int userId, String topic);
+
+    // 查询某个主题所包含的通知列表
+    List<Message> selectNotices(int userId, String topic, int offset, int limit);
 }
