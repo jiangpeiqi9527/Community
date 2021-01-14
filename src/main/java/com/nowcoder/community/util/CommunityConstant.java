77d078dc84706e96b243289c6738641d6767a746
jxz <1741585872@qq.com>
2021-01-14 22:42:20 +0800

    # 置顶、加精、删除
    > 功能实现
    >
    > * 点击置顶，修改帖子的类型
    > * 点击"加精"、"删除"，修改帖子的状态
    >
    > 权限管理
    >
    > * 版主可以执行"置顶"，"加精"操作。
    > * 管理员可以执行"删除"操作
    >
    > 按钮显示
    >
    > * 版主可以看到"置顶"、"加精"按钮
    > * 管理员可以看到"删除"按钮

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -62,6 +62,11 @@ public interface CommunityConstant {
     String TOPIC_PUBLISH = "publish";
 
     /**
+     * 主题：删除帖子
+     */
+    String TOPIC_DELETE = "delete";
+
+    /**
      * 系统用户ID
      */
     int SYSTEM_ID = 1;
